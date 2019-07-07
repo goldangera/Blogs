a3470a7b64227e111cb08ce096041b851aa8a156
goldangera <nkirotegolda6888@gmail.com>
2019-07-07 22:05:31 +0300

    made a few changes to the manage.py file

--- config.py
+++ config.py
@@ -1,9 +1,24 @@
 
 class Config:
-    pass
+    '''
+    General config parent class
+    '''
+    SQLALCHEMY_TRACK_MODIFICATIONS= True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://golda:123@localhost/blogapp'
+
+    # email configs
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
-    pass
+     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     pass
