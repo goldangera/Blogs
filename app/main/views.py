c24082b46f0d19e18995dcce0705059efd5b6e28
goldangera <nkirotegolda6888@gmail.com>
2019-07-07 18:48:57 +0300

    created the main route to the index.html and and set the random quotes to be displayed on the home page

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,22 @@
-from flask import render_template, request, redirect, url_for,abort
 from . import main
+from ..models import User,Post,Comment
 from .. import db
+from .forms import PostForm,CommentForm
+from flask import render_template,redirect,url_for,abort
+from flask_login import login_required,current_user
+from ..email import mail_message
+import datetime
+import json 
+import requests
 
 @main.route('/')
 def index():
-    return '<h1> Hello World </h1>'
+    '''
+    view root page function that returns index page & data
+    '''
+    posts = Post.query.order_by(Post.date_posted.desc()).limit(3).all()
+
+    title = 'Home - Welcome to the SUPER BLOG POST '
+    
+    random=requests.get('http://quotes.stormconsultancy.co.uk/random.json').json()
+    return render_template('index.html',index=index, title=title, post=post, random=random)
