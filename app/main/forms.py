adf0a71ac226bc70507b2c86c9c78d7df2880a23
goldangera <nkirotegolda6888@gmail.com>
2019-07-07 18:37:02 +0300

    created a class to allow users to add their blog post and submit

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
+
+class PostForm(FlaskForm):
+
+    title = StringField('Title', validators=[Required()])
+    text = TextAreaField('Text', validators=[Required()])
+    submit = SubmitField('Submit')
+
