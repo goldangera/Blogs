d397bc5b01cb9a8d43af08e13f0e48933c000ebd
goldangera <nkirotegolda6888@gmail.com>
2019-07-07 19:12:38 +0300

    created  a function to update the user profile and add its info to the database

--- app/main/views.py
+++ app/main/views.py
@@ -20,3 +20,33 @@ def index():
     
     random=requests.get('http://quotes.stormconsultancy.co.uk/random.json').json()
     return render_template('index.html',index=index, title=title, post=post, random=random)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    user_joined = user.date_joined.strftime('%b %d, %Y')
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,date = user_joined)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+   user = User.query.filter_by(username = uname).first()
+   if user is None:
+       abort(404)
+
+   form = UpdateProfile()
+
+   if form.validate_on_submit():
+       user.bio = form.bio.data
+
+       db.session.add(user)
+       db.session.commit()
+
+       return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
